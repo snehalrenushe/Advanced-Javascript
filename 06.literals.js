7c5b8847ff38c61b535f5b5ab79ff6e4f5052f12
snehalrenushe <snehalrenushe15@gmail.com>
2024-05-28 22:57:49 +0530

    literals done with basic let examples

---
+++ 06.literals.js
@@ -0,0 +1,17 @@
+//literals
+
+let id = 101;
+let name = "Snehal";
+let isStudent = false;
+
+let address = `heel
+kjfa`;
+
+let details = `Id: ${id}
+Name: ${name}
+isStudent: ${isStudent}`;
+
+let detailsV2 = "Id: " + id + "\nName: " + name + "\nisStudent: " + isStudent;
+
+console.log(details);
+console.log(detailsV2);
