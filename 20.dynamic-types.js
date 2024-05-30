39afef5bbf48a4f9e6f70bb0e69a6fa0aa54354a
snehalrenushe <snehalrenushe15@gmail.com>
2024-05-30 11:36:54 +0530

    dynamic type example added

---
+++ 20.dynamic-types.js
@@ -0,0 +1,18 @@
+//dynamic types
+
+let n = 101;
+console.log(typeof n);
+n = "Snehal";
+console.log(typeof n);
+n = {
+  id: 101,
+};
+console.log(typeof n);
+
+let n1 = 1;
+let n2 = 3;
+n2 = {
+  id: 101,
+};
+
+console.log(n1 + n2);
