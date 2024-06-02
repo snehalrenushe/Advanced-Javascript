9a10b180f51a6a60b698b90c2dc546047040329f
snehalrenushe <snehalrenushe15@gmail.com>
2024-06-02 18:42:00 +0530

    Math object methods added

---
+++ 59.math-object.js
@@ -0,0 +1,23 @@
+//Math objects
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.LOG10E);
+console.log(Math.SQRT2);
+console.log(Math.LN2);
+console.log(Math.SQRT1_2);
+
+console.log(Math.sign(-2)); // O/P : -
+
+console.log(Math.pow(2, 4));
+console.log(Math.sqrt(49));
+
+console.log(Math.round(4.9)); //O/P : 5
+console.log(Math.floor(3.33305)); //O/P : 3
+console.log(Math.ceil(3.3)); //O/P : 4
+console.log(Math.trunc(8.3444)); //O/P : 8
+
+let arr = [1, 2, 3, 4, 5];
+
+console.log(Math.max(...arr));
+console.log(Math.min(...arr));
