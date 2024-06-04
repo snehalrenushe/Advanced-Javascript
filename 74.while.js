6d1996006f379b0e1d6a81fad8865421cee156e0
snehalrenushe <snehalrenushe15@gmail.com>
2024-06-04 20:07:07 +0530

    do & dowhile added

---
+++ 74.while.js
@@ -0,0 +1,15 @@
+//while
+
+// let n = 1;
+// while (n <= 3) {
+//   console.log(n);
+//   n++;
+// }
+
+let arr = [10, 20, 30];
+
+let index = 0;
+while (index <= arr.length - 1) {
+  console.log(index);
+  index++;
+}
---
+++ 75.do-while.js
@@ -0,0 +1,8 @@
+//do while
+
+let n = 1;
+
+do {
+  console.log(n);
+  n++;
+} while (n <= 3);
