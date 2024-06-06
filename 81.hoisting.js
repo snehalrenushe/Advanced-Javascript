553b368fe3056491dff3ea1fc076e694ec455280
snehalrenushe <snehalrenushe15@gmail.com>
2024-06-06 21:42:07 +0530

    Hoisting function added

---
+++ 81.hoisting.js
@@ -0,0 +1,18 @@
+//Hoisting - function
+
+display();
+
+// function display(){
+//     console.log("display() called...");
+// }
+
+// let display = function () {
+//   console.log("display() called..");
+// };
+
+let display = () => {
+    console.log("display() called..");
+}
+
+
+//Hoisting is only applicable in traditional methods not in annonymous and arrow function
